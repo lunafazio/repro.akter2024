e84b5ebb738d8e6e7d030871512c5265a87df93a
Luna Fazio <bmfaziol@gmail.com>
2025-10-07 05:24:35 +0200

    add paper ref in docs

--- R/load.R
+++ R/load.R
@@ -2,6 +2,10 @@
 #'
 #' @param measure Must exactly match "OR" or "MD".
 #' @return A tibble with data for the specified measure.
+#' @references
+#' Akter, S., Rahman, M.M., Rouyard, T. et al. A systematic review and network
+#' meta-analysis of population-level interventions to tackle smoking behaviour.
+#' Nat Hum Behav 8, 2367–2391 (2024). <https://doi.org/10.1038/s41562-024-02002-7>
 #' @export
 load_data = function(measure=NULL) {
   stopifnot("Argument must equal \"OR\" or \"MD\"" =
