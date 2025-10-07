d7a00e22636da642b92d43a8a553b9794d76e14b
Luna Fazio <bmfaziol@gmail.com>
2025-10-07 13:42:09 +0200

    add netmeta

--- R/meta.R
+++ R/meta.R
@@ -41,3 +41,27 @@ metaMD = function(data) {
     method.random.ci = TRUE
   )
 }
+
+#' Wrapper for the network meta-analysis function.
+#'
+#' Used for network meta-analysis in Figures 2 and 3.
+#' 
+#' Argument names changed according to newer version of `netmeta` package.
+#' Results are unchanged.
+#' 
+#' @param data A tibble with OR data (see `load_data`).
+#' @return A `netmeta` object.
+#' @export
+netmetaOR = function(data) {
+  netmeta::netmeta(
+    TE = TE, seTE = seTE,
+    treat1 = treat1,  # Policy
+    treat2 = treat2,  # Control 
+    studlab = studyid, data = data,
+    sm = "OR",
+    common = FALSE, random = TRUE,
+    reference.group = "Control",
+    details.chkmultiarm = TRUE,
+    sep.trts = " vs "
+  )
+}
